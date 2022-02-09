cd048ee3040de22c44f39fad8a7cb4b3c97e1dcb
Matt Harrell <matthew.629@hotmail.com>
2022-02-08 23:08:32 -0500

    made the JS filter. Now I need to convert to php

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 let categories = [];
+// category = document.querySelector('#addCatInput').value;
 function addCat(category) {
     // const addCatInput = document.querySelector('#addCatInput');
     category = document.querySelector('#addCatInput').value;
+    // filt = String(filt);
     const catButtonWrap = document.querySelector('.cat-wrap');
-    let catButton = `<button>${category}</button>`;
+    let catButton = `<button onclick="filterVids('${category}')" id="${category}" style="display:block;">${category}</button>`;
     
 
     catButtonWrap.insertAdjacentHTML('afterbegin',catButton);
     categories.push(category);
+    // return category;
 }
-
+function filterVids(filt){     
+    let filter = document.querySelectorAll(`.${filt}`);
+    filter.forEach(element => {
+        if (element.style.display === "none") {
+            element.style.display = "block";
+          } else {
+            element.style.display = "none";
+          }          
+        
+    });
+    
+}
